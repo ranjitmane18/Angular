33dc4dd00883e618a4ca809c5f1eff95f66d769d
Ranjit Mane <ranjitmane18@gmail.com>
2022-07-10 00:28:49 +0530

    added new listing page

--- My_Workspace/My_Test_Projects/Test_Create_Host_Angular_App/src/app/new-listing-page/new-listing-page.component.ts
+++ My_Workspace/My_Test_Projects/Test_Create_Host_Angular_App/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
   styleUrls: ['./new-listing-page.component.css']
 })
+
 export class NewListingPageComponent implements OnInit {
 
-  constructor() { }
+  name:string = '';
+  description:string ='';
+  price: string = '';
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() : void {
+    alert(`Creating a new listings page with name : ${this.name} , price : ${this.price} and description : ${this.description}!!`);
+    this.router.navigateByUrl('/my-listing');
+  }
 }
